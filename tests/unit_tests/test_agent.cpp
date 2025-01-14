ad6f6009ec1f9368c7ec5efd34dea95e1a580a53
gordonzu <gordonzu@outlook.com>
2025-01-14 16:19:13 -0500

    initial commit

---
+++ tests/unit_tests/test_agent.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+//#include "agent.h"
+
+using namespace::testing;
+
+class AgentTest: public Test { };
+
+TEST_F(AgentTest, testRandomVacuumAgent) {
+	//Agent* agent = RandomVacuumAgent();
+	//TrivialVacuumEnvironment env{};
+	//env.add_object(agent);
+	//env.run();
+	//std::cout << print(env.status) << "\n";
+	//ASSERT_STREQ(env.status.c_str())
+	//assert env.status == {(1, 0): 'Clean', (0, 0): 'Clean'}
+	EXPECT_EQ (0, 0);
+}
